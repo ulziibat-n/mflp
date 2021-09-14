407c46bd69d8cc4d8dd394e7768b0cc75341adee
ulziibat.n <ulziibat.n@gmail.com>
2021-09-14 16:40:26 +0800

    Таалалд нийцүүлэв

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 import './scss/style.scss';
+function setSectionHeight() {
+    const section = document.querySelector( 'section .container' );
+    section.style.height = (window.innerHeight - document.querySelector( 'header' ).offsetHeight - document.querySelector( 'footer' ).offsetHeight) + 'px';
+}
+setSectionHeight();
+window.onresize = setSectionHeight;
 console.log('NEW FUNKGIN WEB COMING SOON...')
